7cfe5464932daf2ca02619149b06d183f4dff59f
zgfif <zizhag@gmail.com>
2025-01-08 11:09:43 +0200

    add switching color theme

--- app/index.js
+++ app/index.js
@@ -2,83 +2,83 @@ import './styles.css';
 
 import { calculate } from './js/calculate.js';
 import { changeSign, percent } from './js/ariphmetic_functions.js';
+import { toggleTheme } from './js/toggle_theme.js';
 
-document.addEventListener('DOMContentLoaded', function() {
-    const btns = document.querySelectorAll('.btn');
-    let expression = [];
-    let currentNumber = '0';
+document.addEventListener('DOMContentLoaded', function () {
+  const btns = document.querySelectorAll('.btn');
+  const themeBtn = document.querySelector('#themeButton');
 
-    for (const btn of btns) {
-        btn.addEventListener('click', function(event) {
-            let typeOfButton = event.target.getAttribute('data-type'),
-                value = event.target.getAttribute('data-value');
-                       
-            if (typeOfButton == 'operation') {
-                if (currentNumber != '') {
+  let expression = [];
+  let currentNumber = '0';
 
-                    expression.push(currentNumber);
-                    expression.push(value);
-                    currentNumber = '';
+  for (const btn of btns) {
+    btn.addEventListener('click', function (event) {
+      let typeOfButton = event.target.getAttribute('data-type'),
+        value = event.target.getAttribute('data-value');
 
-                    showOnScreen(expression + currentNumber);
-                } else {
-                    expression[expression.length - 1] = value;
-                    showOnScreen(expression);
-                }
+      if (typeOfButton == 'operation') {
+        if (currentNumber != '') {
+          expression.push(currentNumber);
+          expression.push(value);
+          currentNumber = '';
 
-            } else if (typeOfButton == 'number') {
-                
-                if (currentNumber == '0') {
-                    currentNumber = value;
-                } else {
-                    currentNumber += value;
-                };
-                showOnScreen(expression + currentNumber);
-            
-            } else if (typeOfButton == 'point') {
-                if (currentNumber !='' && !currentNumber.includes('.')) {
-                    currentNumber += value;
-                    showOnScreen(expression + currentNumber);
-                }
-            } else if (typeOfButton == '=') {
-                if (currentNumber != '') { expression.push(currentNumber); }
-                
-                let result;
-                result = calculate(expression);
-                expression = [];
-                currentNumber = result + '';
-                
-                showOnScreen(result + '');         
+          showOnScreen(expression + currentNumber);
+        } else {
+          expression[expression.length - 1] = value;
+          showOnScreen(expression);
+        }
+      } else if (typeOfButton == 'number') {
+        if (currentNumber == '0') {
+          currentNumber = value;
+        } else {
+          currentNumber += value;
+        }
+        showOnScreen(expression + currentNumber);
+      } else if (typeOfButton == 'point') {
+        if (currentNumber != '' && !currentNumber.includes('.')) {
+          currentNumber += value;
+          showOnScreen(expression + currentNumber);
+        }
+      } else if (typeOfButton == '=') {
+        if (currentNumber != '') {
+          expression.push(currentNumber);
+        }
 
-            } else if (typeOfButton == 'ac') {
-                expression = [];
-                currentNumber = '0';
-                clearScreen();
-            } else if (typeOfButton == '+-') {
+        let result;
+        result = calculate(expression);
+        expression = [];
+        currentNumber = result + '';
 
-                if (currentNumber != '0') {
-                    let res;
-                    res = changeSign(parseFloat(currentNumber));
-                    currentNumber = res;  
-                    showOnScreen(res);
-                }
-            } else if (typeOfButton == '%') {
-                let res = '';
-                res = percent(parseFloat(currentNumber));
-                currentNumber = res;
-                showOnScreen(res);
-            }
-        });
-    }
+        showOnScreen(result + '');
+      } else if (typeOfButton == 'ac') {
+        expression = [];
+        currentNumber = '0';
+        clearScreen();
+      } else if (typeOfButton == '+-') {
+        if (currentNumber != '0') {
+          let res;
+          res = changeSign(parseFloat(currentNumber));
+          currentNumber = res;
+          showOnScreen(res);
+        }
+      } else if (typeOfButton == '%') {
+        let res = '';
+        res = percent(parseFloat(currentNumber));
+        currentNumber = res;
+        showOnScreen(res);
+      }
+    });
+  }
 
+  themeBtn.addEventListener('click', toggleTheme);
 });
 
-
 function clearScreen() {
-    document.querySelector('#input_field').textContent = '0';
+  document.querySelector('#input_field').textContent = '0';
 }
 
-function showOnScreen(text='') {
-    if (text != '') { document.querySelector('#input_field').textContent = text; }
+function showOnScreen(text = '') {
+  if (text != '') {
+    document.querySelector('#input_field').textContent = text;
+  }
 }
-
